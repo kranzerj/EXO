8d51fe9ae16e68f100c346ecb164702fb6c12903
kranzerj <kranzerj@gmail.com>
2024-05-28 16:14:38 +0200

    Update export_all_mailbox_addresses.ps1

--- export_all_mailbox_addresses.ps1
+++ export_all_mailbox_addresses.ps1
@@ -1,5 +1,6 @@
 #Install-Module -Name ExchangeOnlineManagement
 #Set-ExecutionPolicy RemoteSigned
 Connect-ExchangeOnline -UserPrincipalName your-email@domain.com
- 
- Get-Recipient -ResultSize Unlimited | Where-Object {$_.PrimarySmtpAddress -like '*@*'} | ForEach-Object { $_.EmailAddresses | Where-Object { $_.PrefixString -eq 'SMTP' } | ForEach-Object { [pscustomobject]@{ DisplayName = $_.DisplayName; PrimarySmtpAddress = $_.PrimarySmtpAddress; SMTPAddress = $_.AddressString.Trim('}') } } } | Export-Csv -Path "C:\__addresen.csv" -NoTypeInformation -Encoding UTF8
+
+Get-Mailbox -ResultSize Unlimited | ForEach-Object { $primary = $_.PrimarySmtpAddress; $others = $_.EmailAddresses | Where-Object {$_ -like "SMTP:*" -and $_ -ne "SMTP:$primary"} | ForEach-Object {$_ -replace "SMTP:"}; [PSCustomObject]@{DisplayName = $_.DisplayName; PrimarySMTPAddress = $primary; OtherSMTPAddresses = ($others -join "; ")} } | Export-Csv -Path "C:\__\addresen.csv" -NoTypeInformation
+
